485946aff9bdfcdf327f67b3fab825500d66b64c
Tim Evans <tim.evans@junctionnetworks.com>
2012-02-17 14:18:01 -0500

    removed extra files that were possibly making github not run jekyll on oxjs

--- src/services/preferences.js
+++
@@ -1,8 +0,0 @@
-/**
- * @namespace
- * Namespace for preferences related services. This service has not been implemented yet.
- * @extends OX.Base
- * @extends OX.Mixin.Subscribable
- * @requires A 'connectionAdapter' property inherited from an {@link OX.Connection}.
- */
-OX.Service.Preferences = OX.Base.extend(OX.Mixin.Subscribable, /** @lends OX.Service.Preferences */{});
--- test/rhino.js
+++
@@ -1,7 +0,0 @@
-_load('../frameworks/rhino_dom.js');
-
-_load('../frameworks/yui-test/yahoo-dom-event.js');
-_load('../frameworks/yui-test/yuitest-min.js');
-
-_load('../src/ox.js');
-_load('oxtest.js');
